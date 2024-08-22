13f707043e81eb90f5e0e9ccab6f0a1f6914cfc8
arpegx <arpegx@posteo.de>
2024-08-22 22:29:36 +0200

    WPLN-16-Plant.Index : btn_show on each item

--- resources/js/Pages/Plant/Index.tsx
+++ resources/js/Pages/Plant/Index.tsx
@@ -4,12 +4,17 @@ import { Plant } from "@/types/plant";
 import { Link } from "@inertiajs/react";
 
 export default function Index(plants: Array<Plant>[]) {
-    let list = plants[0].map((plant: Plant, index: number) => (
-        <p key={index}>{plant.nick_name}</p>
+    const plantList = plants[0].map((plant: Plant, index: number) => (
+        <div key={index} className="grid grid-cols-2">
+            <p>{plant.nick_name}</p>
+            <PrimaryButton className="w-24">
+                <Link href={route("plant.show", [{ id: plant.id }])}>show</Link>
+            </PrimaryButton>
+        </div>
     ));
     return (
         <BaseLayout>
-            {plants && list}
+            {plants && plantList}
             <PrimaryButton>
                 <Link href={route("plant.create")}>create</Link>
             </PrimaryButton>
