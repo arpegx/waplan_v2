4d62f31e9c0af7481be5d6abee466b3aff81975e
arpegx <arpegx@posteo.de>
2024-09-08 01:25:07 +0200

    route: redirect to plant.index per default

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\PlantController;
 use Illuminate\Support\Facades\Route;
 
+Route::redirect('/', '/plant');
 Route::resource('/plant', PlantController::class);
 
 require __DIR__ . '/auth.php';
