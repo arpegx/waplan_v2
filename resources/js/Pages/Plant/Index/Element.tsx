2829108e81881587b1f6b071ba8755b72be338d5
arpegx <arpegx@posteo.de>
2024-09-09 23:14:32 +0200

    water: add btn_select to plant.element

--- resources/js/Pages/Plant/Index/Element.tsx
+++ resources/js/Pages/Plant/Index/Element.tsx
@@ -1,15 +1,22 @@
 import PrimaryButton from "@/Components/PrimaryButton";
 import { show } from "@/Helper/Plant";
 import { Plant } from "@/types/plant";
+import { useContext } from "react";
+import { SelectionContext } from "./Index";
 
 interface PropType {
     plant: Plant;
 }
 
 export default function Element({ plant }: PropType) {
+    const select = useContext(SelectionContext);
+    console.log(select.selection);
     return (
-        <div className="grid grid-cols-2 max-h-10">
+        <div className={`grid grid-cols-2 max-h-10 `}>
             <p>{plant.nick_name}</p>
+            <PrimaryButton onClick={() => select.select(plant.id)}>
+                select
+            </PrimaryButton>
             <PrimaryButton
                 onClick={() => show(plant)}
                 className="w-fit justify-self-end"
--- resources/js/Pages/Plant/Index/Index.tsx
+++ resources/js/Pages/Plant/Index/Index.tsx
@@ -3,19 +3,34 @@ import PrimaryButton from "@/Components/PrimaryButton";
 import { create } from "@/Helper/Plant";
 import BaseLayout from "@/Layouts/BaseLayout";
 import { Plant } from "@/types/plant";
+import { createContext, useState } from "react";
 import List from "./List";
 
 interface PropType {
     plants: Plant[];
 }
 
+export const SelectionContext = createContext<any>({
+    id: undefined,
+});
+
 export default function Index({ plants }: PropType) {
+    const [selection, setSelection] = useState([]);
+    const select = (e: never) => {
+        selection.push(e);
+        console.log(selection);
+    };
+
+    const [water, setWater] = useState([]);
     return (
         <BaseLayout>
             <div className="grid content-between h-full">
-                <div className="grid gap-2">
+                <SelectionContext.Provider
+                    value={{ select, selection }}
+                    className="grid gap-2"
+                >
                     {plants && <List plants={plants} />}
-                </div>
+                </SelectionContext.Provider>
                 <ActionBar className="justify-self-end max-h-10">
                     <PrimaryButton onClick={create}>create</PrimaryButton>
                 </ActionBar>
